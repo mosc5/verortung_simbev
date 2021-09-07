fd45bafe7b3051cc39bb943161b4e99259a15351
JakobWeg <jakob.wegner@rl-institute.de>
2021-09-07 10:26:18 +0200

    last update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import configparser as cp
+import os
 
 import Use_Cases
 import Utility
@@ -12,20 +13,20 @@ if __name__ == '__main__':
     print('Starting Program for Distribution of Energy...')
 
     # read config file
+    cwd = os.getcwd()
     parser = cp.ConfigParser()
-    parser.read(r'c:\Users\Jakob.Wegner\PYTHON\verortung_simbev\location_config.cfg')
+    cfg_file = os.path.join(cwd, 'location_config.cfg')
+    if not os.path.isfile(cfg_file):
+        raise FileNotFoundError(f'Config file {cfg_file} not found.')
+    try:
+        parser.read(cfg_file)
+    except Exception:
+        raise FileNotFoundError(f'Cannot read config file {cfg_file} - malformed?')
     uc1_radius = int(parser.get('uc_params', 'uc1_radius'))
-    print(uc1_radius)
-    print(type(uc1_radius))
     uc4_weight_retail = float(parser.get('uc_params', 'uc4_weight_retail'))
     uc4_weight_commercial = float(parser.get('uc_params', 'uc4_weight_commercial'))
     uc4_weight_industrial = float(parser.get('uc_params', 'uc4_weight_industrial'))
-    # num_of_regions = parser.get('region_mode', 'num_of_regions')
 
-    # print('Conifg is set to mode:', mode)
-    # if 'single' in mode:
-    #     region_key = (parser.get('basic', 'region_key'))
-    # elif 'multi' in mode:
     region_data = Utility.load_csv(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\regions.csv')
     region_key = ['']*len(region_data)
     i = 0
@@ -38,33 +39,32 @@ if __name__ == '__main__':
     print('AGS Region_Key is set to:', region_key)
 
     # read input data
-    fuel_stations = Utility.einlesen_geo(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\fuel_stations.gpkg')
+    fuel_stations = Utility.einlesen_geo(os.path.join('.', 'Data', 'fuel_stations.gpkg'))
     anz_fs = len(fuel_stations)
 
-    boundaries = Utility.einlesen_geo(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\boundaries.gpkg')
+    boundaries = Utility.einlesen_geo(os.path.join('.', 'Data', 'boundaries.gpkg'))
 
     boundaries.set_index('ags_0', inplace=True)     # AGS als Index des Dataframes setzen
     boundaries = boundaries.dissolve(by='ags_0')    # Zusammenfassen der Regionenn mit geleichem AGS
     boundaries = boundaries.to_crs(3035)
 
-    traffic = Utility.einlesen_geo(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\berlin_verkehr.gpkg')
+    traffic = Utility.einlesen_geo(os.path.join('.', 'Data', 'berlin_verkehr.gpkg'))
     traffic = traffic.to_crs(3035)  # transforme to reference Coordinate System
 
     zensus_data = Utility.einlesen_geo(
-        r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\destatis_zensus_population_per_ha_filtered.gpkg')
+        os.path.join('.', 'Data', 'destatis_zensus_population_per_ha_filtered.gpkg'))
     zensus_data = zensus_data.to_crs(3035)
     zensus = zensus_data.iloc[:, 2:5]
 
-    public = Utility.einlesen_geo(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\osm_poi_elia.gpkg')
+    public = Utility.einlesen_geo(os.path.join('.', 'Data', 'osm_poi_elia.gpkg'))
 
-    poi_data = Utility.load_csv(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\2020-12-02_OSM_POI_Gewichtung.csv')
+    poi_data = Utility.load_csv(os.path.join('.', 'Data', '2020-12-02_OSM_POI_Gewichtung.csv'))
     poi = pd.DataFrame.from_dict(poi_data)
 
-    work = Utility.einlesen_geo(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\landuse.gpkg')
+    work = Utility.einlesen_geo(os.path.join('.', 'Data', 'landuse.gpkg'))
 
-    am_data = Utility.load_csv(r'C:\Users\Jakob.Wegner\PYTHON\verortung_simbev\Data\RES_SimBEV\amenity.csv')
+    am_data = Utility.load_csv(os.path.join('.', 'Data', 'Res_SimBEV', 'amenity_update.csv'))
     amenities = pd.DataFrame.from_dict(am_data)
-
     anz_regions = len(boundaries)
 
     # Start the Use Cases for areas in region_key
@@ -74,9 +74,9 @@ if __name__ == '__main__':
         region = gpd.GeoSeries(region)  # format to geoseries, otherwise problems plotting
 
         # Start Use Cases
-        fs = Use_Cases.uc1_public_fast(fuel_stations, boundaries,
-                                       amenities, traffic,
-                                       region, region_key[i], uc1_radius)
+        # fs = Use_Cases.uc1_public_fast(fuel_stations, boundaries,
+        #                                amenities, traffic,
+        #                                region, region_key[i], uc1_radius)
 
         pu = Use_Cases.uc2_public_slow(public, boundaries,
                                        amenities, poi,
